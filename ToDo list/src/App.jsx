0599e299e2f528152e680af0059edfe0c31d2101
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-17 23:30:58 +0530

    Added header content

--- ToDo list/src/App.jsx
+++ ToDo list/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import InputsBar from './components/InputsBar/InputsBar';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <InputsBar />
     </>
   )
 }
 
-export default App
+export default App;
---
+++ ToDo list/src/components/InputsBar/InputsBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./InputsBar.css";
+
+const InputsBar = () => {
+  return (
+    <div className='Header-container'>
+      <h3 className='Title-Name'>My ToDo</h3>
+      <form className='Input-Form'>
+        <input type='text' placeholder='Enter task Name'></input>
+        <input type='text' placeholder='Enter Description'></input>
+        <button>Add to list</button>
+      </form>
+    </div>
+  )
+}
+
+export default InputsBar;
