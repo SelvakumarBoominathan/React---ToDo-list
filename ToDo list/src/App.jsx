58597d91f42da2e45e5a018874f7cb5fcd83caad
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-19 00:19:30 +0530

    Dropdown folder removed

--- ToDo list/src/App.jsx
+++ ToDo list/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import InputsBar from './components/InputsBar/InputsBar';
 import Body from './components/Body/Body';
-import Dropdown from './components/Dropdown/Dropdown'
+
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <>
       <InputsBar />
       <Body />
-      <Dropdown />
+
     </>
   )
 }
