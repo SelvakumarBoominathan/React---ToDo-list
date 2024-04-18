d87dfa91fdd5588088419eab9967b270db67c055
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-19 00:13:54 +0530

    new "dropdown' folder added

--- ToDo list/src/components/Body/Body.jsx
+++ ToDo list/src/components/Body/Body.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import "./Body.css"
 
 const Body = () => {
   return (
-    // <div className='parent-container' >
-    <Card style={{ width: '18rem' }} className='card-parent'>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card >
-    // </div>
+    <div className='parent-container' >
+      <Card style={{ width: '18rem' }} className='card-parent'>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card >
+    </div>
 
   )
 
